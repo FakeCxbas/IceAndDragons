5ba78d0707a589e253acf0dfaa11d59b7ef95f0b
dragoncxbas <dragoncxbas@DESKTOP-P5UJHOJ>
2026-09-19 12:48:11 -0500

    fix: redirigir salida de playit a playit.log

--- start_playit.sh
+++ start_playit.sh
@@ -11,7 +11,7 @@ if [ ! -x "./playit" ]; then
 fi
 
 echo "Iniciando demonio Playit.gg..."
-nohup ./playit -l playit.log > /dev/null 2>&1 &
+nohup ./playit > playit.log 2>&1 &
 PID=$!
 echo "Playit iniciado con PID: $PID"
 echo "Puedes ver el enlace de reclamo en playit.log o en el Panel Web."
